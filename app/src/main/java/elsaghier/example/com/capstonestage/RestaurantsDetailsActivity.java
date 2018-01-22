c0a6c4423c2f81e265d55856d8277f77de6ef7a7
M_ELSaghier <thedreamereng@gmail.com>
2018-01-23 00:50:12 +0200

    implement UI of Details of Hotels

--- app/src/main/java/elsaghier/example/com/capstonestage/RestaurantsDetailsActivity.java
+++ app/src/main/java/elsaghier/example/com/capstonestage/RestaurantsDetailsActivity.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class HotelDetailsActivity extends AppCompatActivity {
+public class RestaurantsDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hotel_details);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_restaurants_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
