3c1ac199d4386e5ccb0434c61a17fa6455a4ec84
M_ELSaghier <thedreamereng@gmail.com>
2018-01-23 00:40:02 +0200

    implement UI of Details of Restaurants

---
+++ app/src/main/java/elsaghier/example/com/capstonestage/HotelDetailsActivityFragment.java
@@ -0,0 +1,22 @@
+package elsaghier.example.com.capstonestage;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class HotelDetailsActivityFragment extends Fragment {
+
+    public HotelDetailsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hotel_details, container, false);
+    }
+}
