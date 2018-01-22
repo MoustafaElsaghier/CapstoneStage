54b54049f0595a951cc021ce8ba786a0eefa2656
M_ELSaghier <thedreamereng@gmail.com>
2018-01-23 00:13:42 +0200

    implement UI of Restaurants

---
+++ app/src/main/java/elsaghier/example/com/capstonestage/RestaurantActivity.java
@@ -0,0 +1,18 @@
+package elsaghier.example.com.capstonestage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class RestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
