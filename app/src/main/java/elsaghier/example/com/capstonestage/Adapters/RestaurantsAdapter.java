c628b39d927a3d0186bf2a2bf17cafd26ab103f7
M_ELSaghier <thedreamereng@gmail.com>
2018-01-25 00:46:23 +0200

    add Internet permission & retrofit with gson compilation code

---
+++ app/src/main/java/elsaghier/example/com/capstonestage/Adapters/RestaurantsAdapter.java
@@ -0,0 +1,11 @@
+package elsaghier.example.com.capstonestage.Adapters;
+
+/**
+ * Created by ELSaghier on 1/25/2018.
+ */
+
+public class RestaurantsAdapter {
+
+    // 55a1d18014dd0c0dac534c02598a3368
+
+}
